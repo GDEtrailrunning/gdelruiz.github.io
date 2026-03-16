ae8ac127bed85b83d39b51ea7be593afd372b086
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2026-03-15 22:20:04 -0300

    Subo semana 1 y 2

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
- document.addEventListener("DOMContentLoaded", function () {
-  //actualizarDiasRestantes();
-  //setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
+document.addEventListener("DOMContentLoaded", function () {
+// actualizarDiasRestantes();
+// setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
    // Datos para el gráfico de líneas
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 30',
-    data: [13, 12, 14], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 1',
+    data: [9, 9, 16], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,28 +16,33 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 29',
-    data: [8, 8, 8], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 2',
+    data: [9, 11, 22], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
   }]
 };
 
- // Opciones comunes para los gráficos
- const commonOptions = {
-  responsive: false, // Desactiva el ajuste automático
-    maintainAspectRatio: false, // Permite ajustar proporciones
+// Configuración del gráfico de líneas
+const configLine = {
+  type: 'line',
+  data: dataLine1, // Puede cambiar esto a dataLine2 para mostrar el gráfico de la segunda sección
+  options: {
     scales: {
-      y: { beginAtZero: true }
+      y: {
+        beginAtZero: true
+      }
     }
-  };
+  }
+};
 
 // Seleccione los contextos de los <canvas> y cree los gráficos
 const myChartLine1 = new Chart(document.getElementById('myChart-line-1'), {
   type: 'line',
   data: dataLine1,
   options: {
+    responsive: true, // Esto asegura que el gráfico se ajuste a diferentes tamaños de pantalla
     scales: {
       y: {
         beginAtZero: true
@@ -58,38 +63,18 @@ const myChartLine2 = new Chart(document.getElementById('myChart-line-2'), {
   }
 });
 
-
-  // Ajustar tamaño de los gráficos
-  const canvas1 = document.getElementById('myChart-line-1');
-  canvas1.width = 1000; // Ancho deseado
-  canvas1.height = 500; // Alto deseado
-
-  const canvas2 = document.getElementById('myChart-line-2');
-  canvas2.width = 1000; // Ancho deseado
-  canvas2.height = 500; // Alto deseado
-
-
 function actualizarDiasRestantes() {
-  // Obtener el título que contiene la fecha objetivo
-  const pageTitle =  document.getElementById('pageTitle').innerText;
-    
-  // Extraer la fecha de la cadena del título
-  const objetivoDate = new Date(pageTitle.split(": ")[1]);
-
-  // Obtener la fecha actual
+  // Fecha objetivo fija
+  const objetivoDate = new Date('2025-11-20');
   const currentDate = new Date();
-
-  // Calcular la diferencia en milisegundos entre las dos fechas
   const timeDiff = objetivoDate - currentDate;
-
-  // Calcular la cantidad de días
-  const daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
-
-  // Mostrar el resultado
-  const resultElement = document.getElementById('result');
-  resultElement.textContent = `Faltan ${daysDiff} días para llegar a la carrera.`;
-  console.log("actualizarDiasRestantes function called");
-  }
+  const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeDiff / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((timeDiff / (1000 * 60)) % 60);
+  const seconds = Math.floor((timeDiff / 1000) % 60);
+  const countdownElement = document.getElementById('countdown');
+  countdownElement.innerHTML = `<span class="cuenta-regresiva-titulo">CUENTA REGRESIVA</span><br><span class="cuenta-regresiva-numero">${days}d : ${hours}h : ${minutes}m : ${seconds}s</span>`;
+}
 
   // Crear dos nuevas instancias de HTMLVideoElement para cada sección
   let Semana_1_video_1 = document.createElement("video");
@@ -127,6 +112,4 @@ function actualizarDiasRestantes() {
   document.getElementById("Semana 2").appendChild(video4);
 
 });
-
-
 
